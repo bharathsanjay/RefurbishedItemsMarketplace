a06006d659e7c8fe800ffaf052eacd265bb3b7ef
bharath sanjay <bharathsanjay4321@gmail.com>
2023-06-23 17:36:43 -0400

    homepage doing

---
+++ client/src/pages/ProductInfo/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GetProducts } from '../../apicalls/products';
+
+function ProductInfo() {
+    const [product, setProduct] = React.useState(null);
+   
+  return (
+    <div>ProductInfo</div>
+  )
+}
+
+export default ProductInfo
--- client/src/pages/profile/Products/index.js
+++ client/src/pages/profile/Products/index.js
@@ -20,7 +20,7 @@ function Products() {
                                          });
       dispatch(SetLoader(false));
       if (response.success) {
-        setProducts(response.products);
+        setProducts(response.data);
       }
     } catch (error) {
       dispatch(SetLoader(false));
